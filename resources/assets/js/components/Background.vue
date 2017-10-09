5220362ad1c8f897232635c3d164089b0588bcbc
marre421 <marcus.nordin421@hotmail.com>
2017-10-09 15:11:09 +0200

    started with particlejs

---
+++ resources/assets/js/components/Background.vue
@@ -0,0 +1,22 @@
+<script>
+import particles from "particles.js";
+export default {
+
+    data() {
+        return {
+
+        }
+    },
+
+    created() {
+		particlesJS.load('particles-js', 'assets/particles.json', function() {
+		  console.log('callback - particles.js config loaded');
+		});
+    },
+
+    methods: {
+
+
+    }
+};
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+
+<background inline-template>
+	<div id="particles-js" class="background"></div>
+</background>
+
+@stop
