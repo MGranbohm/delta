7c547bfc2cd07afda820d0babac21b914194cc0c
marre421 <marcus.nordin421@hotmail.com>
2017-10-15 13:28:56 +0200

    frontend

--- resources/assets/js/components/Background.vue
+++ resources/assets/js/components/Background.vue
@@ -1,40 +1,48 @@
 <script>
-import particles from "particles.js";
-import axios from "axios";
+    import particles from "particles.js";
+    import axios from "axios";
 
-export default {
+    export default {
+        props: ['posturl'],
 
-    data() {
-        return {
-        	message: '',
-        	mood: '',
+        data() {
+            return {
+                message: '',
+                mood: '',
+            }
+        },
+
+        created() {
+            particlesJS.load('particles-js', 'assets/particles.json', function() {
+                console.log('callback - particles.js config loaded');
+            });
+        },
+
+        methods: {
+            read(){
+                if(this.message === "fuck you") {
+                    this.mood = "anger";
+                    this.message = "No, Fuck you!"
+                }
+                let msg = new SpeechSynthesisUtterance(this.message);
+                window.speechSynthesis.speak(msg);
+            },
+
+            post() {
+                axios.post(this.posturl +"/message" + this.updateId, this.getFormData())
+                    .then(response => {
+                        console.log(response);
+                    }).catch(error => {
+                    this.isLoading = false;
+                    this.errors = error.response.data.errors;
+                });
+            },
+
+            getData() {
+                return {
+                    message: this.message,
+                }
+            },
         }
-    },
-
-    created() {
-		particlesJS.load('particles-js', 'assets/particles.json', function() {
-		  console.log('callback - particles.js config loaded');
-		});
-    },
-
-    methods: {
-    	read(){
-    		if(this.message === "fuck you") {
-    			this.mood = "anger";
-    			this.message = "No, Fuck you!"
-    		}
-    		let msg = new SpeechSynthesisUtterance(this.message);
-			window.speechSynthesis.speak(msg);
-    	},
-
-        post() {
-    	    axios.post(this.posturl, this.getData())
-                .then(function (response) {
-                    
-                }).catch(function (errors) {
-                
-                })
-        }
-    }
-};
+    };
 </script>
