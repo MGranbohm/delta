396564b12a1a53cab8881b20b9b07ac5382eb153
MGranbohm <granbohm@live.se>
2017-10-15 16:27:35 +0200

    WatsonController

---
+++ app/Http/Controllers/WatsonContoller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Watson\WatsonAPI;
+use Illuminate\Support\Facades\DB;
+use App\Context;
+
+/**
+ * Class that handles message communication with
+ * IBM Watson Conversation API using cURL.
+ *
+ * Class WatsonController
+ * @package App\Http\Controllers
+ */
+class WatsonController extends Controller
+{
+    private $apiConnection;
+
+    public function __construct()
+    {
+        $this->apiConnection = new WatsonAPI();
+    }
+    public function getResponse($input)
+    {
+        $response = $this->apiConnection->getMessage($input);
+        return view('watson', compact('response'));
+    }
+}
